a8e0c9d71bf83b54a7f12241bf7c580d8cd99c34
den <den@192.168.0.100>
2015-03-15 22:43:14 +0400

    1

--- pet-clinic/grails-app/controllers/org/grails/samples/ClinicController.groovy
+++ pet-clinic/grails-app/controllers/org/grails/samples/ClinicController.groovy
@@ -9,15 +9,5 @@ class ClinicController {
 	def vets() {
 		[vets: Vet.list()]
 	}
-	
-	def users() {
-		[users: User.list()]
-	}
-	
-	def delete() {
-		for(Iterator itr = params.findAll().iterator(); itr.hasNext();){
-			String key = itr.next();
-			System.out.println "->" + key;
-		}
-	}
+
 }
